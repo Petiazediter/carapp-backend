40466b205a756563eb3709184999e587c411eed5
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-08-10 11:57:52 +0200

    Reduce boilercode.

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,28 +1,8 @@
 import pubsub from '../pubsub.js';
 
 const createCar = (parent, args, context, info) => {
-	const name = args.name;
-	const brand = args.brand;
-	const model = args.model;
-	const minBid = args.minBid;
-	const country = args.country;
-	const city = args.city;
-	const km = args.km;
-	const body = args.body;
-	const driveTrain = args.driveTrain;
-	const transmission = args.transmission;
-
 	const myCar = {
-		name,
-		brand,
-		model,
-		minBid,
-		country,
-		city,
-		km,
-		body,
-		driveTrain,
-		transmission,
+		...args,
 	};
 
 	pubsub.publish('CAR_CREATED', myCar);
