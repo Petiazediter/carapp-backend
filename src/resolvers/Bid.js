dc752cf948aa0835b4262a49805c2fedac6e7c34
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-08-12 15:48:37 +0200

    Use relation functions when resolve user.

--- src/resolvers/Bid.js
+++ src/resolvers/Bid.js
@@ -1,14 +1,14 @@
 const buyer = async (parent, _, context) => {
-	const userController = context.userController;
-	const buyerId = parent.userId;
-	if (buyerId) {
-		return await userController.findUserById(buyerId);
-	}
+	const bidController = context.bidController;
+	const bid = await bidController.getBidById(parent.id);
+	if (!bid) return null;
+	return bid.getUser();
 };
 
 const car = async (parent, _, context) => {
 	const bidController = context.bidController;
 	const bid = await bidController.getBidById(parent.id);
+	if (!bid) return null;
 	return bid.getCar();
 };
 
