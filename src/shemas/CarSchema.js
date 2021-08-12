1d7663d0703d2a67373bc426a991b46f41cfbebd
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-08-12 11:02:57 +0200

    Fix mutation to create car.

--- src/shemas/CarSchema.js
+++ src/shemas/CarSchema.js
@@ -3,34 +3,34 @@ import { gql } from 'apollo-server';
 const schema = gql`
 	type Car {
 		id: ID!
-		name: String
-		sellerId: Int
+		name: String!
+		sellerId: Int!
 		seller: User
-		brand: String
-		model: String
-		minBid: Int
-		country: String
-		city: String
-		vin: String
-		km: Int
-		body: Body
-		driveTrain: DriveTrain
-		transmission: Transmission
-		exterior: String
-		interior: String
-		highlightsTitle: String
-		highlightsItems: [String]
-		equipmentTitle: String
-		equipmentItems: [String]
-		flaws: [String]
-		serviceHistroy: String
-		extraItems: [String]
-		ownerShipHistory: String
-		videos: [String]
-		exteriorImages: [String]
-		interiorImages: [String]
-		paperImages: [String]
-		bids: [Bid]
+		brand: String!
+		model: String!
+		minBid: Int!
+		country: String!
+		city: String!
+		vin: String!
+		km: Int!
+		body: Body!
+		driveTrain: DriveTrain!
+		transmission: Transmission!
+		exterior: String!
+		interior: String!
+		highlightsTitle: String!
+		highlightsItems: [String!]!
+		equipmentTitle: String!
+		equipmentItems: [String!]!
+		flaws: [String!]!
+		serviceHistroy: String!
+		extraItems: [String!]!
+		ownerShipHistory: String!
+		videos: [String!]!
+		exteriorImages: [String!]!
+		interiorImages: [String!]!
+		paperImages: [String!]!
+		bids: [Bid!]!
 	}
 
 	enum Transmission {
--- src/shemas/MutationSchema.js
+++ src/shemas/MutationSchema.js
@@ -4,15 +4,31 @@ const mutations = gql`
 	type Mutation {
 		createCar(
 			name: String!
+			sellerId: Int!
 			brand: String!
 			model: String!
 			minBid: Int!
 			country: String!
 			city: String!
+			vin: String!
 			km: Int!
 			body: Body!
 			driveTrain: DriveTrain!
 			transmission: Transmission!
+			exterior: String!
+			interior: String!
+			highlightsTitle: String!
+			highlightsItems: [String!]!
+			equipmentTitle: String!
+			equipmentItems: [String!]!
+			flaws: [String!]!
+			serviceHistroy: String!
+			extraItems: [String!]!
+			ownerShipHistory: String!
+			videos: [String!]!
+			exteriorImages: [String!]!
+			interiorImages: [String!]!
+			paperImages: [String!]!
 		): Car
 
 		deleteCar(id: Int!): String!
