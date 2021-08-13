bfe6a11701559ad836d360ef991b094972b175ff
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-08-13 12:24:29 +0200

    Add relations between cars and images.

--- src/main.js
+++ src/main.js
@@ -16,6 +16,7 @@ import process from 'process';
 import { BidController } from './controllers/BidController.js';
 import { getIdFromToken } from './utils/jwt.js';
 import { UserController } from './controllers/UserController.js';
+import { ImageController } from './controllers/ImageController.js';
 
 process.on('beforeExit', () => {
 	console.log('👋️ Bye bye! Exit application!');
@@ -26,32 +27,45 @@ const resolvers = { Query, Car, Mutation, Subscription, Bid, User };
 const createRelations = async (
 	userController,
 	carController,
-	bidController
+	bidController,
+	imageController
 ) => {
 	const Cars = carController.getCarsTable();
 	const Bids = bidController.getBidsTable();
 	const Users = userController.getUsersTable();
+	const Images = imageController.getImagesTable();
 
-	await Cars.sync();
-	await Users.sync();
-	await Bids.sync();
+	await Promise.all([Cars.sync(), Users.sync(), Bids.sync(), Images.sync()]);
 
+	// Link many bids to one car.
 	Cars.hasMany(Bids);
 	Bids.belongsTo(Cars);
 
-	Cars.hasMany(Bids);
+	// Link many bids to one user
+	Users.hasMany(Bids);
 	Bids.belongsTo(Users);
 
+	// Link many car to one user
 	Users.hasMany(Cars);
 	Cars.belongsTo(Users);
+
+	// Link many image to one car
+	Cars.hasMany(Images);
+	Images.belongsTo(Cars);
 };
 
 (async () => {
 	const userController = new UserController();
 	const carController = new CarController();
 	const bidController = new BidController();
+	const imageController = new ImageController();
 
-	await createRelations(userController, carController, bidController);
+	await createRelations(
+		userController,
+		carController,
+		bidController,
+		imageController
+	);
 
 	const app = express();
 	const httpServer = createServer(app);
