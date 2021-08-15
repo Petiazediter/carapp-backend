6d9d2e86d3677b451aa077b225b6414579f0ffd6
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-08-15 19:07:19 +0200

    Create anserController.

---
+++ src/controllers/AnswerController.js
@@ -0,0 +1,36 @@
+import { getConnection } from './Database.js';
+import Sequelize from 'sequelize';
+
+export class AnswerController {
+	constructor() {
+		this.connection = getConnection();
+		this.answers = this.connection.define('answers', {
+			id: {
+				type: Sequelize.INTEGER,
+				field: 'id',
+				autoIncrement: true,
+				primaryKey: true,
+				allowNull: false,
+			},
+			userId: {
+				type: Sequelize.INTEGER,
+				field: 'userId',
+				allowNull: false,
+			},
+			commentId: {
+				type: Sequelize.INTEGER,
+				field: 'commentId',
+				allowNull: false,
+			},
+			answer: {
+				type: Sequelize.STRING,
+				field: 'answer',
+				allowNull: false,
+			},
+		});
+	}
+
+	getAnswersTable() {
+		return this.answers;
+	}
+}
