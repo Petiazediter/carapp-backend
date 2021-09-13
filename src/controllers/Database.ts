4b52ae933a53c66f232ef0ed03a8f037e7c930ae
Peti <49650243+Petiazediter@users.noreply.github.com>
2021-09-13 18:35:08 +0200

    Implement dotenv for hide database parameters.

--- src/controllers/Database.ts
+++ src/controllers/Database.ts
@@ -1,13 +1,20 @@
 import { Sequelize } from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
 
-const sequelize = new Sequelize('car_app_backend', 'root', 'peter133', {
-	host: 'localhost',
-	dialect: 'mysql',
-	pool: {
-		max: 5,
-		min: 0,
-		idle: 10000,
-	},
-});
+const sequelize = new Sequelize(
+	process.env.DB_NAME ? process.env.DB_NAME : '',
+	process.env.DB_USER ? process.env.DB_USER : '',
+	process.env.DB_PASS ? process.env.DB_PASS : '',
+	{
+		host: process.env.DB_HOST ? process.env.DB_HOST : '',
+		dialect: 'mysql',
+		pool: {
+			max: 5,
+			min: 0,
+			idle: 10000,
+		},
+	}
+);
 
 export const getConnection = () => sequelize;
